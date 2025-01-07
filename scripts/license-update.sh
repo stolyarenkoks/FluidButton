71c57eca991b16182ede3eba128e6f61fca56075
Konstantin Stolyarenko <stolyarenko.kons@gmail.com>
2025-01-07 13:47:34 +0100

    Updated script and cron

--- scripts/license-update.sh
+++ scripts/license-update.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-# Получаем полную дату в формате: "January 7, 2025"
+# Get full date: "January 1, 2025"
 FULL_DATE=$(date +"%B %d, %Y")
 
-# Обновляем LICENSE файл, заменяя только дату
+# Update LICENSE file, replacing only the date
 sed -i "s/Copyright © [A-Za-z]* [0-9]*, [0-9]*\(.*\)$/Copyright © $FULL_DATE\1/" LICENSE
-echo "Updated LICENSE file:"
-cat LICENSE  # Отладка: выводим содержимое LICENSE
 
-# Обновляем README.md файл, заменяя только дату
+# Update README.md file, replacing only the date
 sed -i "s/Copyright © [A-Za-z]* [0-9]*, [0-9]*\(.*\)$/Copyright © $FULL_DATE\1/" README.md
-echo "Updated README.md file:"
-cat README.md  # Отладка: выводим содержимое README.md
